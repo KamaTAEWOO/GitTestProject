acec42461fbde30ee8c583ec92d6a8a13f464cd6
KamaTAEWOO <zszs2020@naver.com>
2024-12-19 21:52:14 +0900

    init

---
+++ app/src/main/java/com/example/aproject/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.example.aproject
+
+import android.annotation.SuppressLint
+import android.content.ComponentName
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val button = findViewById<Button>(R.id.btnMove)
+        button.setOnClickListener {
+            val intent = Intent().apply {
+                component = ComponentName(
+                    "com.example.bproject",
+                    "com.example.bproject.ui.settings.MainActivity"
+                )
+                addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+            }
+
+            try {
+                startActivity(intent)
+            } catch (e: Exception) {
+                e.printStackTrace()
+                // 액티비티를 찾지 못한 경우 처리
+                Toast.makeText(this, "${e.printStackTrace()}", Toast.LENGTH_SHORT).show()
+            }
+
+        }
+    }
+}
