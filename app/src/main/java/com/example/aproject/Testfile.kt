da4bcc6868b74cf6141dfc1cbb9ff50708095174
KamaTAEWOO <zszs2020@naver.com>
2025-01-05 22:35:12 +0900

    수정

--- app/src/main/java/com/example/aproject/Testfile.kt
+++ app/src/main/java/com/example/aproject/Testfile.kt
@@ -1,7 +1,6 @@
 package com.example.aproject
 
 class Testfile {
-    // This is a test file c
-    // This is a test file c
-    // This is a test file c
+    // This is a test file a
+    // This is a test file b
 }
