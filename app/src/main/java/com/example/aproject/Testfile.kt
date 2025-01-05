dd73b30e26ddbe3e97d7cde4071b79f34ba75d38
KamaTAEWOO <zszs2020@naver.com>
2025-01-05 22:22:53 +0900

    after reset add

--- app/src/main/java/com/example/aproject/Testfile.kt
+++ app/src/main/java/com/example/aproject/Testfile.kt
@@ -1,27 +1,4 @@
 package com.example.aproject
 
 class Testfile {
-    // This is a test file_a
-    // This is a test file_b
-    // This is a test file_c
-    // This is a test file_d
-    // This is a test file_e
-    // This is a test file_a
-    // This is a test file_b
-    // This is a test file_c
-    // This is a test file_d
-    // This is a test file_e
-    // This is a test file_f
-    // This is a test file_a
-    // This is a test file_b
-    // This is a test file_c
-    // This is a test file_d
-    // This is a test file_e
-    // This is a test file_f
-    // This is a test file_a
-    // This is a test file_b
-    // This is a test file_c
-    // This is a test file_d
-    // This is a test file_e
-    // This is a test file_f
 }
