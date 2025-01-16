4098cd8f180d195a005b3eb8ad66ed2267aa1dea
KamaTAEWOO <zszs2020@naver.com>
2025-01-16 22:47:43 +0900

    test 완료

--- app/src/main/java/com/example/aproject/Testfile.kt
+++ app/src/main/java/com/example/aproject/Testfile.kt
@@ -2,14 +2,4 @@ package com.example.aproject
 
 class Testfile {
     // This is a test file n
-    // This is a test file a
-    // This is a test file b
-
-
-
-
-
-
-    // This is a test file f
-    // This is a test file g
 }
