ef39801082638591c517aa661d9f593fa9f0eed7
KamaTAEWOO <zszs2020@naver.com>
2025-01-19 16:43:07 +0900

    test 추가

--- app/src/main/java/com/example/aproject/Testfile.kt
+++ app/src/main/java/com/example/aproject/Testfile.kt
@@ -1,4 +1,15 @@
 package com.example.aproject
 
 class Testfile {
+    fun test() {
+        println("Hello, World!")
+    }
+
+    fun test2() {
+        println("Hello, World!")
+    }
+
+    fun test3() {
+        println("Hello, World!")
+    }
 }
